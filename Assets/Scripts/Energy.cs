82c4f4baccb2f3789aed402a54bedb218e473a1f
Sandra Keßler <mail@sandra-kessler.net>
2023-04-13 00:19:26 +0200

    energy

---
+++ Assets/Scripts/Energy.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour
+{
+    private Image image;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+
+        GameManager.OnEnergy += OnEnergy;
+    }
+
+    private void OnEnergy(object sender, GameManager.EnergyEventArgs e)
+    {
+        image.fillAmount = e.energy / e.maxEnergy;
+    }
+}
